b1984961d5b31023512648f54aa6461743aac237
Stefan Krenz <krenz.stefan@googlemail.com>
2024-01-09 12:05:45 +0100

    Downgrade spatie/phpunit-snapshot-assertions

---
+++ tests/Makaira/Connect/Modifier/Product/__snapshots__/VariantAttributesModifierTest__testFethcesAttributesFromProductWithoutVariants.php
@@ -0,0 +1,83 @@
+<?php return \Makaira\Connect\Type\Product\Product::__set_state(array(
+   'es_id' => NULL,
+   'id' => 'phpunit_product',
+   'timestamp' => NULL,
+   'url' => NULL,
+   'active' => true,
+   'shop' => 
+  array (
+  ),
+   'additionalData' => 
+  array (
+  ),
+   'attributeStr' => 
+  array (
+  ),
+   'attributeInt' => 
+  array (
+  ),
+   'attributeFloat' => 
+  array (
+  ),
+   'isPseudo' => false,
+   'ean' => '',
+   'title' => '',
+   'searchkeys' => '',
+   'hidden' => false,
+   'sort' => 0,
+   'longdesc' => '',
+   'shortdesc' => '',
+   'stock' => 0,
+   'onstock' => false,
+   'manufacturerid' => '',
+   'manufacturer_title' => '',
+   'price' => 0.0,
+   'insert' => NULL,
+   'soldamount' => 0,
+   'rating' => 0.0,
+   'searchable' => true,
+   'picture_url_main' => 
+  array (
+  ),
+   'attributes' => 
+  array (
+    0 => 
+    array (
+      'intAttr' => 1337,
+      'floatAttr' => 13.37,
+      'stringAttr' => 'phpunit',
+    ),
+  ),
+   'parent' => '',
+   'meta_keywords' => NULL,
+   'meta_description' => NULL,
+   'selfLinks' => 
+  array (
+  ),
+   'maincategory' => NULL,
+   'maincategoryurl' => NULL,
+   'mak_boost_norm_insert' => 0.0,
+   'mak_boost_norm_sold' => 0.0,
+   'mak_boost_norm_rating' => 0.0,
+   'mak_boost_norm_revenue' => 0.0,
+   'mak_boost_norm_profit_margin' => 0.0,
+   'isVariant' => false,
+   'activeto' => NULL,
+   'activefrom' => NULL,
+   'suggest' => 
+  array (
+  ),
+   'category' => 
+  array (
+  ),
+   'TRACKING' => NULL,
+   'tmpAttributeStr' => 
+  array (
+  ),
+   'tmpAttributeInt' => 
+  array (
+  ),
+   'tmpAttributeFloat' => 
+  array (
+  ),
+));
