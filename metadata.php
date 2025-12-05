396740852eb3c77020dbfbaf786c338c6a9f06df
MaximilianRohr <102803077+MaximilianRohr@users.noreply.github.com>
2025-12-05 12:19:52 +0000

    feat(Settings): Add opt-out to personalization feature (#65)
    * feat(Settings): Add opt-out to personalization feature
    
    * fix(Composer): Update CICD to use composer2

--- metadata.php
+++ metadata.php
@@ -155,6 +155,12 @@ $aModule = [
             'type'  => 'bool',
             'value' => true,
         ],
+        [
+            'group' => 'OPERATIONAL_INTELLIGENCE',
+            'name'  => 'makaira_connect_personalization_enabled',
+            'type'  => 'bool',
+            'value' => true,
+        ],
         ['name'  => 'makaira_connect_use_odoscope',
          'group' => 'OPERATIONAL_INTELLIGENCE',
          'type'  => 'bool',
